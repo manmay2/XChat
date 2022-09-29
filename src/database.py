384291a817232bb2a87e80da6e0c90f10c600bf5
SoumadeepChoudhury <chanchalchoudhury.2018@gmail.com>
2022-09-30 00:27:51 +0530

    fixed bug#1 lost connection error

--- src/database.py
+++ src/database.py
@@ -4,20 +4,21 @@ from SignUp import *
 import os
 print("........WELCOME TO THE WORLD OF XCHAT APP.........")
 try:
-    mycon = s.connect(host="localhost", user=os.environ.get("DB_USER"),
-                      passwd=os.environ.get("DB_PASS"), database="xchat")
-    cursor = mycon.cursor()
+    mycon = s.connect(host=os.environ.get("DB_FOREIGN_SERVER"), user=os.environ.get("DB_FOREIGN_USER"),
+                      passwd=os.environ.get("DB_FOREIGN_PASS"), database=os.environ.get("DB_FOREIGN_USER"))
+    if mycon.is_connected():
+        cursor = mycon.cursor()
 
-    cursor.execute(
-        "create table if not exists signup(username varchar(200) primary key,password varchar(8))")
+        cursor.execute(
+            "create table if not exists signup(username varchar(200) primary key,password varchar(8))")
 
-    print("1. NEW TO THE APP!! PRESS 1 TO SIGN UP")
-    print("2. ALREADY HAVE AN ACCOUNT !! PRESS 2 TO SIGN IN")
+        print("1. NEW TO THE APP!! PRESS 1 TO SIGN UP")
+        print("2. ALREADY HAVE AN ACCOUNT !! PRESS 2 TO SIGN IN")
 
-    choice = int(input("ENTER YOUR CHOICE: "))
-    if(choice == 1):
-        signup(mycon, cursor)
-    elif(choice == 2):
-        signin(cursor, mycon)
+        choice = int(input("ENTER YOUR CHOICE: "))
+        if(choice == 1):
+            signup(mycon, cursor)
+        elif(choice == 2):
+            signin(cursor, mycon, s)
 except:
     print("Something error occured...Try after sometime")
--- src/main.py
+++ src/main.py
@@ -1,72 +1,113 @@
+import os
 import threading
-STOP=False
-MSG=[]
-def multi(cursor,mycon,user_id,chat_user,table_name):
+MSG = []
+STOP = False
+
+
+def multi(cursor, mycon, user_id, chat_user, table_name, s):
     global STOP
     try:
-        push_thread=threading.Thread(target=push,args=(cursor,mycon,user_id,table_name))
-        fetch_thread=threading.Thread(target=fetch,args=(cursor,mycon,table_name,chat_user,user_id))
+        push_thread = threading.Thread(
+            target=push, args=(cursor, mycon, user_id, table_name, s))
+        fetch_thread = threading.Thread(
+            target=fetch, args=(cursor, mycon, table_name, user_id, chat_user, s))
         push_thread.start()
         fetch_thread.start()
         push_thread.join()
         fetch_thread.join()
     except:
         print("Logged Out")
-        STOP=True
-def push(cursor,mycon,user_id,table_name):
+        STOP = True
+
+
+def push(cursor, mycon, user_id, table_name, s):
     global STOP
     from datetime import datetime
-    print(f"{user_id}: ",end='')
-    text=input()
-    if text.strip()!='':
+    print(f"{user_id}: ", end='')
+    text = input()
+    if text.strip() != '':
         try:
-            cursor.execute("insert into {}({}) values('{}');".format(table_name,user_id,text+" "+str(datetime.now())))
+            text = str(text+" "+str(datetime.now()))
+            cursor.execute(
+                f'insert into {table_name}({user_id}) values("{text}");')
             mycon.commit()
         except:
-            STOP=True
-            print("Error occured.. \nLogged Out")
+            if not mycon.is_connected():
+                try:
+                    mycon = s.connect(host=os.environ.get("DB_FOREIGN_SERVER"), user=os.environ.get("DB_FOREIGN_USER"),
+                                      passwd=os.environ.get("DB_FOREIGN_PASS"), database=os.environ.get("DB_FOREIGN_USER"))
+                    cursor = mycon.cursor()
+                    cursor.execute(
+                        f'insert into {table_name}({user_id}) values("{text}");')
+                    mycon.commit()
+                except:
+                    STOP = True
+                    print("Error occured........Try again later...\nLogged Out")
+            else:
+                print("Error occured........Try again later...\nLogged Out")
+                STOP = True
     else:
-        print("\x1b[1F",end='')
+        print("\x1B[1F", end='')
     if not STOP:
-        push(cursor,mycon,user_id,table_name)
-def fetch(cursor,mycon,table_name,chat_user,userid):
-    global MSG,STOP
+        push(cursor, mycon, user_id, table_name, s)
+
+
+def fetch(cursor, mycon, table_name, user_id, chat_user, s):
     import time
+    global MSG, STOP
     try:
-        cursor.execute("select {} from {};".format(chat_user,table_name))
+        cursor.execute(f"select {chat_user} from {table_name};")
         for i in cursor:
             if STOP:
                 break
             if i == (None,):
                 continue
             if i[0] not in MSG:
-                print("\r\x1b[2k",end='')
-                print(f"{chat_user}: {' '.join(i[0].split()[0:-2])}\n{userid}: ",end='')
+                print("\r\x1b[2K", end='')
+                print(
+                    f"{chat_user}: {' '.join(i[0].split()[0:-2])}\n{user_id}: ", end='')
                 MSG.append(i[0])
         time.sleep(0.5)
         mycon.commit()
     except:
-        print("Error in fetching data.\n Logged Out")
-        STOP=True
+        if not mycon.is_connected():
+            try:
+                mycon = s.connect(host=os.environ.get("DB_FOREIGN_SERVER"), user=os.environ.get("DB_FOREIGN_USER"),
+                                  passwd=os.environ.get("DB_FOREIGN_PASS"), database=os.environ.get("DB_FOREIGN_USER"))
+                cursor = mycon.cursor()
+                mycon.commit()
+            except:
+                STOP = True
+                print("Error occured in Fetching Data..\nLogged Out")
+        else:
+            print("Error occured in Fetching Data..\nLogged Out")
+            STOP = True
     if not STOP:
-        fetch(cursor,mycon,table_name,chat_user,userid)
-def fetchall(cursor,user_id,chat_user,table_name):
+        fetch(cursor, mycon, table_name, user_id, chat_user, s)
+
+
+def fetchall(cursor, user_id, chat_user, table_name):
     global MSG
-    user_id_msg=[]
-    chat_user_msg=[]    
-    cursor.execute("select {},{} from {};".format(user_id,chat_user,table_name))
-    for i in cursor:
-        if i==(None,None):
-            continue
-        user_id_msg.append(i[0])
-        chat_user_msg.append(i[1])
-    if user_id_msg!=[] or chat_user_msg!=[]:
-        for i in range(len(user_id_msg)):
-            if user_id_msg==[]:
-                break
-            if user_id_msg[i]==None:
-                print(f"{chat_user}:",' '.join(chat_user_msg[i].split()[0:-2]))
-            if chat_user_msg[i]==None:
-                print(f"{user_id}:",' '.join(user_id_msg[i].split()[0:-2]))
-    MSG=chat_user_msg
-    
+    user_id_msg = []
+    chat_user_msg = []
+    try:
+        cursor.execute("select {},{} from {};".format(
+            user_id, chat_user, table_name))
+        for i in cursor:
+            if i == (None, None):
+                continue
+            user_id_msg.append(i[0])
+            chat_user_msg.append(i[1])
+        if user_id_msg != [] or chat_user_msg != []:
+            for i in range(len(user_id_msg)):
+                if user_id_msg == []:
+                    break
+                if user_id_msg[i] == None:
+                    print(f"{chat_user}:", ' '.join(
+                        chat_user_msg[i].split()[0:-2]))
+                if chat_user_msg[i] == None:
+                    print(f"{user_id}:", ' '.join(
+                        user_id_msg[i].split()[0:-2]))
+        MSG = chat_user_msg
+    except:
+        print("Some error occured...Try again later\nLogged Out")
